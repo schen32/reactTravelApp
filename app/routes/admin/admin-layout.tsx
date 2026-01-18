ea8cf838294c83ea64d8e1618c76683786466d4d
Simon Chen <simonc8192@gmail.com>
2026-01-18 11:27:57 -0800

    - navigation sidebar - user image and details - logout button

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import NavItems from "components/NavItems";
+import MobileSidebar from "components/MobileSidebar";
 
 const adminLayout = () => {
   return (
     <div className="admin-layout">
-      MobileSidebar
-      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+      <MobileSidebar></MobileSidebar>
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems></NavItems>
+        </SidebarComponent>
+      </aside>
       <aside className="children">
         <Outlet></Outlet>
       </aside>
---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const allUsers = () => {
+  return <div>Users</div>;
+};
+
+export default allUsers;
