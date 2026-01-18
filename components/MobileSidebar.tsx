2b21ff7d7df43cfcf4c1c7cef693ff67137726e5
Simon Chen <simonc8192@gmail.com>
2026-01-18 13:45:27 -0800

    - mobile sidebar

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,7 +1,32 @@
+import type { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 import React from "react";
+import { Link } from "react-router";
 
 const MobileSidebar = () => {
-  return <div>Mobile Sidebar</div>;
+  let sidebar: SidebarComponent;
+
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header>
+        <Link to="/">
+          <img
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            className="size-[30px]"
+          />
+        </Link>
+
+        <h1>Tourvisto</h1>
+        <button
+          onClick={() => {
+            sidebar.toggle();
+          }}
+        >
+          <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
+        </button>
+      </header>
+    </div>
+  );
 };
 
 export default MobileSidebar;
