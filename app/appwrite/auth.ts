c567917f3a825058fe40fb20684b805f991c6fb9
Simon Chen <simonc8192@gmail.com>
2026-01-20 13:36:40 -0800

    - appwrite auth functions

---
+++ app/appwrite/auth.ts
@@ -0,0 +1,103 @@
+import { ID, OAuthProvider, Query } from "appwrite";
+import { account, appwriteConfig, table } from "./client";
+import { redirect } from "react-router";
+
+export const loginWithGoogle = async () => {
+    try {
+        account.createOAuth2Session({
+            provider: OAuthProvider.Google,
+        })
+    } catch (e) {
+        console.error("loginWithGoogle", e);
+    }
+}
+
+export const logoutUser = async () => {
+    try {
+        await account.deleteSession({
+            sessionId: "current"
+        })
+        return true;
+    } catch (e) {
+        console.error("logoutUser", e);
+        return false;
+    }
+}
+
+export const getUser = async () => {
+    try {
+        const user = await account.get();
+        if (!user) return redirect("/sign-in")
+        
+        const { rows } = await table.listRows({
+            databaseId: appwriteConfig.databaseId,
+            tableId: appwriteConfig.userTableId,
+            queries: [
+                Query.equal("accountId", user.$id),
+                Query.select(["name", "email", "imageUrl", "joinedAt", "accountId"])
+            ]
+        })
+        return rows.length > 0 ? rows[0] : redirect("/sign-in");
+    } catch (e) {
+        console.error("getUser", e);
+    }
+}
+
+export const getGooglePicture = async (accessToken: string) => {
+    try {
+        const response = await fetch(
+          "https://people.googleapis.com/v1/people/me?personFields=photos",
+          { headers: { Authorization: `Bearer ${accessToken}` } }
+        );
+        if (!response.ok) throw new Error("Failed to fetch Google profile picture");
+    
+        const { photos } = await response.json();
+        return photos?.[0]?.url || null;
+      } catch (error) {
+        console.error("Error fetching Google picture:", error);
+        return null;
+      }
+}
+
+export const storeUserData = async () => {
+    try {
+      const user = await account.get();
+      if (!user) throw new Error("User not found");
+  
+      const { providerAccessToken } = (await account.getSession({ sessionId: "current" })) || {};
+      const profilePicture = providerAccessToken
+        ? await getGooglePicture(providerAccessToken)
+        : null;
+  
+      const createdUser = await table.createRow({
+        databaseId: appwriteConfig.databaseId,
+        tableId: appwriteConfig.userTableId,
+        rowId: ID.unique(),
+        data: {
+          accountId: user.$id,
+          email: user.email,
+          name: user.name,
+          imageUrl: profilePicture,
+          joinedAt: new Date().toISOString(),
+        }
+    });
+  
+      if (!createdUser.$id) redirect("/sign-in");
+    } catch (error) {
+      console.error("Error storing user data:", error);
+    }
+  };
+
+  export const getExistingUser = async (id: string) => {
+    try {
+      const { rows, total } = await table.listRows({
+        databaseId: appwriteConfig.databaseId,
+        tableId: appwriteConfig.userTableId,
+        queries: [Query.equal("accountId", id)]
+    });
+      return total > 0 ? rows[0] : null;
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      return null;
+    }
+  };
---
+++ app/appwrite/client.ts
@@ -0,0 +1,21 @@
+import { Account, Client, Databases, Storage, TablesDB } from "appwrite";
+
+export const appwriteConfig = {
+    endpointUrl: import.meta.env.VITE_APPWRITE_ENDPOINT_URL,
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    apiKey: import.meta.env.VITE_APPWRITE_API_KEY,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    userTableId: import.meta.env.VITE_APPWRITE_USERS_TABLE_ID,
+    tripTableId: import.meta.env.VITE_APPWRITE_TRIPS_TABLE_ID,
+
+}
+
+const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl!)
+    .setProject(appwriteConfig.projectId!);
+
+const account = new Account(client);
+const table = new TablesDB(client);
+const storage = new Storage(client);
+
+export { client, account, table, storage };
