3844b25c940eced529f99d7b76ee68c02ac0dcda
Simon Chen <simonc8192@gmail.com>
2026-01-24 23:49:51 -0800

    - trips page map - highlight selected country

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -1,6 +1,14 @@
 import { ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
+import {
+  LayerDirective,
+  LayersDirective,
+  MapsComponent,
+} from "@syncfusion/ej2-react-maps";
 import { Header } from "components";
+import { formatKey } from "lib/utils";
 import React, { useEffect } from "react";
+import { comboBoxItems, selectItems } from "~/constants";
+import { world_map } from "~/constants/world_map";
 
 // export const loader = async () => {
 //   const response = await fetch("https://restcountries.com/v3.1/all");
@@ -11,8 +19,28 @@ import React, { useEffect } from "react";
 
 const createTrip = () => {
   const [countries, setCountries] = React.useState<Array<any>>([]);
+  const [formData, setFormData] = React.useState<TripFormData>({
+    country: countries[0]?.name?.common || "",
+    travelStyle: "",
+    interest: "",
+    budget: "",
+    duration: 0,
+    groupType: "",
+  });
 
-  const handleChange = (key: keyof TripFormData, value: string | number) => {};
+  const mapData = [
+    {
+      country: formData.country,
+      color: "#EA382E",
+      coordinates:
+        countries.find((c: Country) => c.name === formData.country)
+          ?.coordinates || [],
+    },
+  ];
+
+  const handleChange = (key: keyof TripFormData, value: string | number) => {
+    setFormData({ ...formData, [key]: value });
+  };
 
   const fetchCountries = async () => {
     try {
@@ -72,6 +100,65 @@ const createTrip = () => {
                 e.updateData(filteredData);
               }}
             ></ComboBoxComponent>
+          </div>
+
+          <div>
+            <label htmlFor="duration">Duration</label>
+            <input
+              id="duration"
+              name="duration"
+              placeholder="Enter a number of days (5, 12, ...)"
+              className="form-input placeholder:text-gray-100"
+              onChange={(e) => handleChange("duration", Number(e.target.value))}
+            ></input>
+          </div>
+
+          {selectItems.map((key) => (
+            <div key={key}>
+              <label htmlFor={key}>{formatKey(key)}</label>
+              <ComboBoxComponent
+                id={key}
+                dataSource={comboBoxItems[key].map((item) => ({
+                  text: item,
+                  value: item,
+                }))}
+                fields={{ text: "text", value: "value" }}
+                placeholder={`Select ${key}`}
+                change={(e: { value: string | undefined }) => {
+                  if (e.value) {
+                    handleChange(key, e.value);
+                  }
+                }}
+                allowFiltering
+                filtering={(e) => {
+                  const query = e.text.toLowerCase();
+
+                  const filteredData = comboBoxItems[key]
+                    .filter((item) => item.toLowerCase().includes(query))
+                    .map((item) => ({
+                      text: item,
+                      value: item,
+                    }));
+
+                  e.updateData(filteredData);
+                }}
+              ></ComboBoxComponent>
+            </div>
+          ))}
+
+          <div>
+            <label htmlFor="location">Location on the world map</label>
+            <MapsComponent>
+              <LayersDirective>
+                <LayerDirective
+                  dataSource={mapData}
+                  shapeData={world_map}
+                  shapePropertyPath="name"
+                  shapeDataPath="country"
+                  shapeSettings={{ colorValuePath: "color", fill: "#e5e5e5" }}
+                ></LayerDirective>
+              </LayersDirective>
+            </MapsComponent>
           </div>
         </form>
       </section>
